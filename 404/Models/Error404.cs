ee5c3c6ac0b3831435ee00db52e09635c2349b69
ashley <kilgourkilgour@hotmail.com>
2016-10-25 22:20:39 +0100

    Added error model and text box for user message

---
+++ 404/Models/Error404.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _404.Models
+{
+    public class Error404
+    {
+        [DisplayName("Please Enter What You Were Doing")]
+        [StringLength(160, MinimumLength = 2)]
+        public string UserMessage { get; set; }
+    }
+}
