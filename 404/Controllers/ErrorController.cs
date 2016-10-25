3311a8cdf35b89be4d9616ac3a1a2d5abc92b5e1
ashley <kilgourkilgour@hotmail.com>
2016-10-25 15:39:39 +0100

    Created Error controller and web.config to handel direction

---
+++ 404/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _404.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: ErrorController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
