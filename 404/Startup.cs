7c4f31454baf79be2936be77605e2e4258d944e6
ashley <kilgourkilgour@hotmail.com>
2016-10-25 01:09:13 +0100

    Add project files.

---
+++ 404/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_404.Startup))]
+namespace _404
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
